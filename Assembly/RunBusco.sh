bc0b623cd975d2175a4fda6570d0fbb755fdb645
Clay <5608468+cdizzel@users.noreply.github.com>
2025-05-12 17:41:26 -0400

    Update RunBusco.sh

--- Assembly/RunBusco.sh
+++ Assembly/RunBusco.sh
@@ -3,3 +3,5 @@
 docker run -u $(id -u) -v $(pwd):/busco_wd ezlabgva/busco:v5.3.2_cv1 \
 busco -i ordered_new_ficifolium_corrected.fa -o UnmaskedCorrected2024embryophyta_odb10 -l embryophyta_odb10/ \
 -m genome -c 22
+
+python tools/busco/scripts/generate_plot.py -wd UnmaskedCorrected2024embryophyta_odb10
